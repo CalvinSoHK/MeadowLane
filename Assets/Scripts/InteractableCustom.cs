558d662e5db04b4ecc63a667d78be87a4f58bf87
Calvin So <ccs403@nyu.edu>
2017-08-17 14:41:54 -0700

    Interactable Custom

---
+++ Assets/Scripts/InteractableCustom.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+//Custom version of Interactable for our game.
+public class InteractableCustom : Interactable{
+
+    //Overrideable use function for all interactable objects
+	public virtual void Use(Hand hand)
+    {
+        //Default interaction is nothing. Debug something
+        Debug.Log(gameObject.name + " Interactable: Has no use function");
+    }
+}
