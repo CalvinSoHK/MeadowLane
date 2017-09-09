cb126c7556b8bb507a8de501249405c10dc0f825
Aleksei Waddington <amw694@nyu.edu>
2017-09-09 17:45:31 -0400

    More work on container

--- Assets/Scripts/Shop/GeneralStore.cs
+++ Assets/Scripts/Shop/GeneralStore.cs
@@ -8,6 +8,7 @@ public class GeneralStore : MonoBehaviour {
 
     public TextAsset storeItemReference;
     public string[] itemArray;
+    public int storeCredit;
     //public string fileName;
 
 	// Use this for initialization
@@ -24,6 +25,16 @@ public class GeneralStore : MonoBehaviour {
 		
 	}
 
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Collider"))
+        {
+            Container tempContainer = collision.gameObject.GetComponent<Container>();
+            //the container shouldn't be able to be picked up anymore.
+            collision.gameObject.GetComponent<InteractionPickup>().enabled = false;
+        }
+    }
     void sellItems(string fileName)
     {
         
