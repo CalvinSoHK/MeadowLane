382219a4dd89f86763c9e82219131d210a422c3a
Calvin So <ccs403@nyu.edu>
2017-09-28 14:04:44 -0700

    Playtest build

--- Assets/InteractableRotator.cs
+++ Assets/InteractableRotator.cs
@@ -5,6 +5,8 @@ using Valve.VR.InteractionSystem;
 
 [RequireComponent(typeof(CircularDriveCustom))]
 //Custom interactactble for rotating use.   
+//On standard button down on the OTHER hand, moves forward
+//Enables rigidbody on move, disables on stop.
 public class InteractableRotator : InteractableCustom {
 
     //The transform that we want to move
@@ -18,6 +20,12 @@ public class InteractableRotator : InteractableCustom {
 
     //Ref for current velocity
     float currentVelocityRot;
+
+    //Ref for Rigidbody
+    public Rigidbody rb;
+
+    //Ref for meshCollider
+    public MeshCollider mc;
 
     //Write use function
     public override void Use(Hand hand)
@@ -41,6 +49,8 @@ public class InteractableRotator : InteractableCustom {
             //If we get the other controller trigger
             if (otherHand.GetStandardInteractionButton())
             {
+                mc.enabled = false;
+                rb.isKinematic = false;
                 //Calculate target forward vector based on yRotation
                 Quaternion targetRot = vehicle.rotation;
                 targetRot = Quaternion.AngleAxis(yRotation, vehicle.up) * targetRot;
@@ -50,6 +60,11 @@ public class InteractableRotator : InteractableCustom {
 
                 //If we move forward
                 vehicle.GetComponent<Rigidbody>().MovePosition(vehicle.position + vehicle.forward * speed);
+            }
+            else
+            {
+                rb.isKinematic = true;
+                mc.enabled = true;
             }
         }
 
