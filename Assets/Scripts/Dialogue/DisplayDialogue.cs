371a36a4962256a2962216981466bc31bf345ccc
Aleksei Waddington <amw694@nyu.edu>
2018-01-13 14:14:44 -0500

    formatting + comments

--- Assets/Scripts/Dialogue/DisplayDialogue.cs
+++ Assets/Scripts/Dialogue/DisplayDialogue.cs
@@ -6,24 +6,23 @@ using System.IO;
 
 public class DisplayDialogue: MonoBehaviour{
 
-    public string characterName, currentSituation;
-    public string[] allSituations;
-    public bool shopOwner;
-    public enum GameState { Wait, Idle, DialogueSetup, Typing, WaitingToProceed, TransitionToShop, StopDisplayingText}
-    public GameState currentState;
-    float time = 0.0f, lastStateChange;    
-    public GameObject textBox;
-    public Text textObject;
+    public string characterName, currentSituation; //strings representing the specific character name and situation for this instance
+    public string[] allSituations; //array of all possible instances for that character
+    public bool shopOwner; //whether or not they are a shop owner
+    public enum GameState { Wait, Idle, DialogueSetup, Typing, WaitingToProceed, TransitionToShop, StopDisplayingText} //statemachine for the displaying of dialogue
+    public GameState currentState; //current state in the state machine
+    float time = 0.0f, lastStateChange; //references to the amount of time that has passed since last state change
+    public GameObject textBox; //reference to the specific textbox for the character
+    public Text textObject; //reference to the specific ui textobject associated to the textbox.
 
-    private int numberOfLines, indexLine, indexLetter;
-    private string currentLine;
-    private bool inDialogue;
+    private int numberOfLines, indexLine, indexLetter; //private ref to the current line, letter, and total line of dialogue that are needed/currently being displayed
+    private string currentLine; //string reference to the current line being displayed
+    private bool inDialogue; //checks whether the character is currently in dialogue
 
 
     // Use this for initialization
     void Start()
     {
-
 
     }
 
@@ -38,9 +37,7 @@ public class DisplayDialogue: MonoBehaviour{
         {
             case GameState.Wait: //if we don't need it to do anything atm
 
-                break;
-
-            
+                break;           
 
             case GameState.DialogueSetup: //Get the correct Dialogue from the dialogue manager based on name and situation.
                 DialogueManager.setUpCurrentDialogue(characterName, currentSituation, shopOwner); //Find the lines of dialogue needed for this character and situation instance
@@ -50,7 +47,7 @@ public class DisplayDialogue: MonoBehaviour{
                 currentLine = ""; //reset the line of dialogue being displayed
                 inDialogue = true; //they are now in dialogue
                 textBox.SetActive(true); //display the text box
-                setCurrentState(GameState.Typing);
+                setCurrentState(GameState.Typing); //setup up is done, we now need to display the text
                 break;
 
             case GameState.Typing: //if dialogue is currently being typed out
@@ -58,7 +55,7 @@ public class DisplayDialogue: MonoBehaviour{
                 {
                     currentLine += DialogueManager.currentDialogueForCharacter[indexLine][indexLetter]; //add the next letter to the current line
                     textObject.text = currentLine;// update the text ui element
-                    indexLetter += 1;
+                    indexLetter += 1; //move on to the next letter
                 }else
                 {
                     setCurrentState(GameState.WaitingToProceed); //once all the letters have been displayed, wait for the player to proceed to the next 
@@ -84,37 +81,22 @@ public class DisplayDialogue: MonoBehaviour{
 
             case GameState.StopDisplayingText: //we need to stop displaying text as we have reached the end of the dialogue
                 textBox.SetActive(false); //turn off the dialogue box
-                textObject.text = "";//remove the text in the text object
-                inDialogue = false; //they are no longer in dialogue
-                if (shopOwner) //check if they are shop owner
+                textObject.text = "";//remove the text in the text object                
+                if (shopOwner && inDialogue) //check if they are shop owner and that the player is still near the character when the conversation ended
                 {
                     setCurrentState(GameState.TransitionToShop); //transition to the shop stuff (NOT YET DONE)
                 }else //if they are not a shop owner
                 {
                     setCurrentState(GameState.Wait);
                 }
-
+                inDialogue = false; //they are no longer in dialogue
                 break;
+
             case GameState.TransitionToShop: //If you are talking to a shop owner have it transition to buy items (maybe not necessary)
 
                 break;           
         }
-    }
-
-    public void DebugTextParsing()
-    {
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            DialogueManager.setUpCurrentDialogue("Aleksei", "TestingCode", false);
-        }
-        if (Input.GetKeyDown(KeyCode.N))
-        {
-            for (int i = 0; i < DialogueManager.currentDialogueForCharacter.Count; i++)
-            {
-                Debug.Log(DialogueManager.currentDialogueForCharacter[i]);
-            }
-        }
-    }
+    }    
 
     /// <summary>
     /// sets the current state of the game manager
@@ -136,5 +118,19 @@ public class DisplayDialogue: MonoBehaviour{
         return Time.time - lastStateChange; //return time since the last change in state
     }
 
-    
+    /*
+    public void DebugTextParsing()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            DialogueManager.setUpCurrentDialogue("Aleksei", "TestingCode", false);
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            for (int i = 0; i < DialogueManager.currentDialogueForCharacter.Count; i++)
+            {
+                Debug.Log(DialogueManager.currentDialogueForCharacter[i]);
+            }
+        }
+    }*/
 }
